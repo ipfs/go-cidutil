07d0223ca81518f084b7e139292fe882e6584ece
Kevin Atkinson <k@kevina.org>
2018-11-25 23:10:58 -0500

    Remove code that depends on `go-path`.
    Also remove other now unused code.

--- cidenc/encoder.go
+++ cidenc/encoder.go
@@ -3,10 +3,7 @@ package cidenc
 import (
 	"context"
 
-	cidutil "github.com/ipfs/go-cidutil"
-
 	cid "github.com/ipfs/go-cid"
-	path "github.com/ipfs/go-path"
 	mbase "github.com/multiformats/go-multibase"
 )
 
@@ -67,91 +64,6 @@ func cidVer(v string) int {
 	} else {
 		return 1
 	}
-}
-
-// FromPath creates a new encoder that is influenced from the encoded
-// Cid in a Path.  For CidV0 the multibase from the base encoder is
-// used and automatic upgrades are disabled.  For CidV1 the multibase
-// from the CID is used and upgrades are eneabled.  On error the base
-// encoder is returned.  If you don't care about the error condiation
-// it is safe to ignore the error returned.
-func FromPath(enc Encoder, p string) (Encoder, error) {
-	v := extractCidString(p)
-	if cidVer(v) == 0 {
-		return Encoder{enc.Base, false}, nil
-	}
-	e, err := mbase.NewEncoder(mbase.Encoding(v[0]))
-	if err != nil {
-		return enc, err
-	}
-	return Encoder{e, true}, nil
-}
-
-func extractCidString(p string) string {
-	segs := path.FromString(p).Segments()
-	v := segs[0]
-	if v == "ipfs" && len(segs) > 0 {
-		v = segs[1]
-	}
-	return v
-}
-
-// WithOverride is like Encoder but also contains a override map to
-// preserve the original encoding of select CIDs
-type WithOverride struct {
-	base     Encoder
-	override map[cid.Cid]string
-}
-
-func (enc WithOverride) Encoder() Encoder {
-	return enc.base
-}
-
-func (enc WithOverride) Map() map[cid.Cid]string {
-	return enc.override
-}
-
-func NewOverride(enc Encoder) WithOverride {
-	return WithOverride{base: enc, override: map[cid.Cid]string{}}
-}
-
-// Add adds a Cid to the override map if it will be encoded
-// differently than the base encoder
-func (enc WithOverride) Add(cids ...string) {
-	for _, p := range cids {
-		v := p
-		c, err := cid.Decode(v)
-		if err != nil {
-			continue
-		}
-		if enc.base.Encode(c) != v {
-			enc.override[c] = v
-		}
-		c2 := cidutil.TryOtherCidVersion(c)
-		if c2.Defined() && enc.base.Encode(c2) != v {
-			enc.override[c2] = v
-		}
-	}
-}
-
-func (enc WithOverride) Encode(c cid.Cid) string {
-	v, ok := enc.override[c]
-	if ok {
-		return v
-	}
-	return enc.base.Encode(c)
-}
-
-func (enc WithOverride) Recode(v string) (string, error) {
-	if len(enc.override) == 0 {
-		return enc.base.Recode(v)
-	}
-	c, err := cid.Decode(v)
-	if err != nil {
-		return v, err
-	}
-
-	return enc.Encode(c), nil
 }
 
 type encoderKey struct{}
